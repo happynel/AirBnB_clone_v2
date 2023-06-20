80ba8dcff490722d59a1f3e0dd99f21a892c07ec
Ay-iman <ayomiderabiu00@gmail.com>
2023-06-20 07:40:44 +0100

    AirBnB

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Implements the Amenity model"""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       viewonly=False)
+
+    else:
+        name = ""
+
+        def __init__(self, *args, **kwargs):
+            """initialize amenities"""
+            super().__init__(*args, **kwargs)
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
-from models.amenity import Amenity
+""" State Module for HBNB project """
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class Amenity(BaseModel, Base):
+    """ represents Amentiy"""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        # place_amenities = relationship("Place", secondary=place_amenity)
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """initalize amentie"""
         super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
-
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
